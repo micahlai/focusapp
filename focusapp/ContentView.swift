98da1067400a271b202e0a9fb777b018f0c7832a
micahlai <micahlai@gmail.com>
2024-08-11 01:54:08 -0600

    Initial Commit

---
+++ focusapp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  focusapp
+//
+//  Created by Micah Lai on 8/11/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ focusapp/focusappApp.swift
@@ -0,0 +1,17 @@
+//
+//  focusappApp.swift
+//  focusapp
+//
+//  Created by Micah Lai on 8/11/24.
+//
+
+import SwiftUI
+
+@main
+struct focusappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
